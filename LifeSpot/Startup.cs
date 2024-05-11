349dfd00791c8f378355f98553d0000bf3ffdd2f
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-05-11 19:48:52 +0300

    30.3.2

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -51,6 +51,13 @@ namespace LifeSpot
                     await context.Response.WriteAsync(html.ToString());
                 });
 
+                endpoints.MapGet("/Static/JS/testing.js", async context =>
+                {
+                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "testing.js");
+                    var js = await File.ReadAllTextAsync(jsPath);
+                    await context.Response.WriteAsync(js);
+                });
+
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
